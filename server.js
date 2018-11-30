a580a07fe2563364a1c075d53884ca75ed622c53
arshij <arshi.jujara98@gmail.com>
2018-11-30 00:20:05 -0800

    Changes to excel functions

--- server.js
+++ server.js
@@ -1,56 +1,3 @@
-/* Sample Input JSON data
-{
-    'process_state': 'LPN Pack for BTS',
-    'application_name': 'Cisco Production Fulfillment',
-    'consumed_exposed': 'Consumed',
-    'track': 'Test Environment - Supply Chain TS3',
-    'subtrack': 'TS3 Supply Chain - Non Prod',
-    'VALUE': 'test.com',
-    'service_offering_name': 'Outsourced Product Fulfillment (1.0)',
-    'process_name': 'BTS Flow',
-    'process': 'Y',
-    'job_name': 'Cisco Make Pack LPN for BTS',
-    'type': 'Concurrent Program',
-    'database': 'TS1,TS3',
-    'related_to': 'Test Environment - Supply Chain TS3',
-    'jvm_name': 'sbpservices-kw88wf3icst1-1',
-    'host': 'lae-rtp1-li-159',
-    'host_env': 'LAE',
-}
-*/
-
-/* On submit, generate field process_state_code by concatenating process_state + track
- * Example:
- * track = Test_Environment - Supply Chain TS3 --> sc_nonprod_ts3
- * process_state = LPN Pack for BTS --> lpn_pack_for_bts
- * process_state_code = lpn_pack_for_bts_sc_nonprod_ts3
-*/
-
-// Add field process = 'Y'
-
-/* Sample Modified Input JSON data
-{
-    'process_state': 'LPN Pack for BTS',
-    'application_name': 'Cisco Production Fulfillment',
-    'consumed_exposed': 'Consumed',
-    'track': 'Test Environment - Supply Chain TS3',
-    'subtrack': 'TS3 Supply Chain - Non Prod',
-    'VALUE': 'test.com',
-    'service_offering_name': 'Outsourced Product Fulfillment (1.0)',
-    'process_name': 'BTS Flow',
-    'process': 'Y',
-    'job_name': 'Cisco Make Pack LPN for BTS',
-    'type': 'Concurrent Program',
-    'database': 'TS1,TS3',
-    'related_to': 'Test Environment - Supply Chain TS3',
-    'jvm_name': 'sbpservices-kw88wf3icst1-1',
-    'host': 'lae-rtp1-li-159',
-    'host_env': 'LAE',
-    'process_state_code': 'lpn_pack_for_bts_sc_nonprod_ts3',
-    'process': 'Y'
-}
-*/
-
 /* MongoDB database information:
  * "dummydatabase": contains the populated data
  * "userinput": contains modified user input data (add 'process_state_code', 'NAME', 'process')
@@ -60,15 +7,13 @@
 var express = require("express");
 var bodyparser = require("body-parser");
 var mongoose = require("mongoose");
-//var MongoClient = require('mongodb').MongoClient;
-
+var MongoClient = require('mongodb').MongoClient;
 var urlencodedParser = bodyparser.urlencoded({extended: false});
 
 var app = express();
 app.set("view engine", "ejs");
 app.use(bodyparser.json());
 
-//var url = "mongodb://localhost:27017/"; // Current URL is local host
 var url = "mongodb://gems:Cisco123@ds261917.mlab.com:61917/gemsdashboard";
 
 mongoose.connect(url, {useNewUrlParser: true});
@@ -200,118 +145,8 @@ app.post("/send", urlencodedParser, function(req, res){
 
 	res.json(tempJson); //Optional. But if you remove this, remove the parameter from the success function in app.js
 });
-
-/*
-// Takes field from the dummy database and returns the entire array
-MongoClient.connect(url, function(err, db) {
-	if (err) throw err;
-	var dbo = db.db("test");
-
-	// Application Name
-	var applicationName = dbo.collection("users").distinct('application_name');
-	applicationName.then(function (item) {
-		console.log(item);
-	});
-
-	// Consumed or Exposed
-	var applicationName = dbo.collection("dummydatabase").distinct('consumed_exposed');
-	applicationName.then(function (item) {
-		console.log(item);
-	});
-
-	// Track
-	var applicationName = dbo.collection("dummydatabase").distinct('track');
-	applicationName.then(function (item) {
-		console.log(item);
-	});
-
-	// Subtrack
-	var applicationName = dbo.collection("dummydatabase").distinct('subtrack');
-	applicationName.then(function (item) {
-		console.log(item);
-	});
-
-	// Service Offering Name
-	var applicationName = dbo.collection("dummydatabase").distinct('service_offering_name');
-	applicationName.then(function (item) {
-		console.log(item);
-	});
-
-	// Type
-	var applicationName = dbo.collection("dummydatabase").distinct('type');
-	applicationName.then(function (item) {
-		console.log(item);
-	});
-
-	// Database
-	var applicationName = dbo.collection("dummydatabase").distinct('database');
-	applicationName.then(function (item) {
-		console.log(item);
-	});
-
-	// Related To
-	var applicationName = dbo.collection("dummydatabase").distinct('related_to');
-	applicationName.then(function (item) {
-		console.log(item);
-	});
-
-	// JVM Name
-	var applicationName = dbo.collection("dummydatabase").distinct('jvm_name');
-	applicationName.then(function (item) {
-		console.log(item);
-	});
-
-	// Host
-	var applicationName = dbo.collection("dummydatabase").distinct('host');
-	applicationName.then(function (item) {
-		console.log(item);
-	});
-
-	// Host Environment
-	var applicationName = dbo.collection("dummydatabase").distinct('host_env');
-	applicationName.then(function (item) {
-		console.log(item);
-	});
-});
-*/
-
-// Modified dummy user input as JSON data sent from UI
-/*
-var input = [{
-    'process_state': 'LPN Pack for BTS',
-    'application_name': 'Cisco Production Fulfillment',
-    'consumed_exposed': 'Consumed',
-    'track': 'Test Environment - Supply Chain TS3',
-    'subtrack': 'TS3 Supply Chain - Non Prod',
-    'VALUE': 'test.com',
-    'service_offering_name': 'Outsourced Product Fulfillment (1.0)',
-    'process_name': 'BTS Flow',
-    'process': 'Y',
-    'job_name': 'Cisco Make Pack LPN for BTS',
-    'type': 'Concurrent Program',
-    'database': 'TS1,TS3',
-    'related_to': 'Test Environment - Supply Chain TS3',
-    'jvm_name': 'sbpservices-kw88wf3icst1-1',
-    'host': 'lae-rtp1-li-159',
-    'host_env': 'LAE',
-    'process_state_code': 'process_state_code',
-}];
-
-MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    var dbo = db.db("gemsdashboard");
-    dbo.collection("userinput").insertMany({input}, function(err, result) {
-        if (err) throw err;
-        console.log(result.name);
-        db.close();
-    });
-});
-*/
-/*
-var MongoClient=require('mongodb').MongoClient;
-var url="mongodb://localhost:27017/test";
 /* Excel Sheets */
-/*
+
 var Excel = require('exceljs');
 
 var jobs = new Excel.Workbook();
@@ -325,34 +160,32 @@ var serviceurlws = serviceurl.addWorksheet('Service URL');
 
 MongoClient.connect(url, function(err, db) {
     if (err) throw err;
-    var dbo = db.db("test");
-    console.log("connected");//connected!
-    var app=dbo.collection("users").find().toArray(function(err,docs){
-        console.log(docs);   });
-
-
-	dbo.collection("users").find().toArray(function(err, docs) {
-		docs.forEach(function(doc) {
-			jobsExcel(doc.app,doc.serviceOff,doc.process_state,doc.process_state,doc.process,doc.jobName,doc.theType,doc.database,doc.process,doc.relTo,jobsws);
-			jvmsExcel(doc.app,doc.jvm,doc.hosts,doc.hostEnv,doc.process_state,doc.process,jvmsws);
-			processFlowExcel(doc.process,doc.process_state,doc.track,doc.sub_tracks,doc.process,processflowws);
-			serviceURLExcel(doc.app,doc.appUrl,doc.consExpo,doc.process,serviceurlws)
-		});
-		writeToExcel(jobs, "Jobs.xlsx");
-		writeToExcel(jvms, "JVMs.xlsx");
-		writeToExcel(processflow, "ProcessFlowStates.xlsx");
-		writeToExcel(serviceurl, "ServiceURL.xlsx");
-		db.close();
+    var dbo = db.db("gemsdashboard");
+    console.log("connected");	//connected!
+    var app=dbo.collection("userinput").find().toArray(function(err,docs){
+        console.log(docs);
 	});
 
-
+	dbo.collection("userinput").find().toArray(function(err, docs) {
+		docs.forEach(function(doc) {
+		jobsExcel(doc.app_name,doc.service_offering_name,doc.process_state_name,doc.process_state_code,doc.process_name,doc.job_name,doc.type,doc.database,doc.process,doc.related_to,jobsws);
+			jvmsExcel(doc.app_name,doc.jvm_name,doc.host,doc.host_env,doc.process_state_code,doc.process,jvmsws);
+			processFlowExcel(doc.process_state_name,doc.process_state_code,doc.track,doc.subtrack,doc.process,processflowws);
+			serviceURLExcel(doc.app_name,doc.app_url,doc.consumed_exposed,doc.process,serviceurlws)
+		});
+		writeToExcel(jobs,"Jobs.xlsx");
+		writeToExcel(jvms,"JVMs.xlsx");
+		writeToExcel(processflow,"ProcessFlowStates.xlsx");
+		writeToExcel(serviceurl,"ServiceURL.xlsx");
+		db.close();
+	});
 });
 
-function jobsExcel(application_name,service_offering_name,process_state,process_state_code,process_name,job_name,type,database,process,related_to,worksheet) {
+function jobsExcel(application_name,service_offering_name,process_state_name,process_state_code,process_name,job_name,type,database,process,related_to,worksheet) {
 	worksheet.columns = [
 		{ header: 'application_name', key: 'application_name', width: 20 },
 		{ header: 'service_offering_name', key: 'service_offering_name', width: 25 },
-		{ header: 'process_state', key: 'process_state', width: 20},
+		{ header: 'process_state_name', key: 'process_state_name', width: 20},
 		{ header: 'process_state_code', key: 'process_state_code', width: 30},
 		{ header: 'process_name', key: 'process_name', width: 15},
 		{ header: 'job_name', key: 'job_name', width: 30},
@@ -361,7 +194,8 @@ function jobsExcel(application_name,service_offering_name,process_state,process_
 		{ header: 'process', key: 'process', width: 10},
 		{ header: 'related_to', key: 'related_to', width: 30}
 	];
-	worksheet.addRow([application_name,service_offering_name,process_state,process_state_code,process_name,job_name,type,database,process,related_to]);
+	worksheet.addRow([application_name,service_offering_name,process_state_name,process_state_code,process_name,job_name,type,database,process,related_to]);
+	
 }
 
 function jvmsExcel(application_name,jvm_name,host,host_env,process_state_code,process,worksheet) {
@@ -376,30 +210,29 @@ function jvmsExcel(application_name,jvm_name,host,host_env,process_state_code,pr
 	worksheet.addRow([application_name,jvm_name,host,host_env,process_state_code,process]);
 }
 
-function processFlowExcel(process_state,process_state_code,track,subtrack,process,worksheet){
+function processFlowExcel(process_state_name,process_state_code,track,subtrack,process,worksheet){
 	worksheet.columns = [
-		{ header: 'process_state', key: 'process_state', width: 20 },
+		{ header: 'process_state_name', key: 'process_state_name', width: 20 },
 		{ header: 'process_state_code', key: 'process_state_code', width: 30 },
 		{ header: 'track', key: 'track', width: 30},
 		{ header: 'subtrack', key: 'subtrack', width: 30},
 		{ header: 'process', key: 'process', width: 10}
 	];
-	worksheet.addRow([process_state,process_state_code,track,subtrack,process]);
+	worksheet.addRow([process_state_name,process_state_code,track,subtrack,process]);
 }
 
-function serviceURLExcel(application_name,application_url,consumed_exposed,process_state,worksheet){
+function serviceURLExcel(application_name,application_url,consumed_exposed,process_state_name,worksheet){
 	worksheet.columns = [
 		{ header: 'application_name', key: 'process_state', width: 20 },
 		{ header: 'NAME', key: 'name', width: 20 },
 		{ header: 'VALUE', key: 'value', width: 20},
-		{ header: 'consumed_exposed', key: 'consumed_exposed', width: 15},
-		{ header: 'process_state', key: 'process_state', width: 10}
+		{ header: 'consumed_exposed', key: 'consumed_exposed', width: 20},
+		{ header: 'process_state_name', key: 'process_state_name', width: 10}
 	];
-	var name = application_name;// =============================================== I added the word "var" here. Is it necessary?
-	worksheet.addRow([application_name,name,application_url,consumed_exposed,process_state]);
+	var name = application_name;
+	worksheet.addRow([application_name,name,application_url,consumed_exposed,process_state_name]);
 }
 
 function writeToExcel(workbook, filename) {
 	workbook.xlsx.writeFile(filename).then(function(){});
 }
-*/
